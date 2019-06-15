068ab8f2ac04402b25b853a6e8d10f2d0277eb8b
Sem - 6 - INGENIERIA INDUSTRIAL: Wilson Alejandro Gonzalez Gaitan <wilsongonzalez.ag@academia.umb.edu.co>
2019-06-15 11:03:18 -0500

    Create test with jest and vue

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -17,6 +17,7 @@ module.exports = {
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
   mapCoverage: true,
+  testURL: 'https://localhost',
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
